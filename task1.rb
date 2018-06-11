4fc84b9956ac088e54047e779216c9673e4af127
AndrewBrains <paa1401@mail.ru>
2018-06-11 12:31:48 +0300

    task1.rb and task2.rb updated

--- task1.rb
+++ task1.rb
@@ -1,15 +1,26 @@
+def normal (x)
+x=x.delete(' ')
+x=x.delete('?')
+x=x.delete('!')
+x=x.delete('.')
+x=x.delete(',')
+x=x.delete('-')
+return x
+end
+
 a = ARGV[0]
 q = 1
-n = a.size 
+a=normal(a)
+a=a.downcase
+n = a.size
 if n == 1
  puts "YES"
 else
  for i in 0..n/2
-  if a[i] != a[n-i-1]
-    q = 0
-	end
-    break if q == 0
- 
+ if a[i] != a[n-i-1]
+ q = 0
+ end
+ break if q == 0
  end
  if q==1
  puts "YES"
--- task2.rb
+++ task2.rb
@@ -2,16 +2,27 @@ a=[]
 a[0]=0
 a[1]=1
 a[2]=1
+n = ARGV[0].to_i
+if n>=0
 n1 = a[1]
 n2 = a[2]
-n = ARGV[0].to_i
 if n<=2
  puts a[n]
 else
  counter = 3
  while counter<=n
-  n1,n2 = n2,n1+n2
-  counter+=1
+ n1,n2 = n2,n1+n2
+ counter+=1
  end
  puts n2
+end
+else
+counter = -1
+n1 = a[0]
+n2 = a[1]
+while counter>=n
+n1,n2=n2-n1,n1
+counter-=1
+end
+puts n1
 end
