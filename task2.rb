bb42307af7cdebe9ea396d2d86aef58304a7eb9d
AndrewBrains <paa1401@mail.ru>
2018-06-16 09:54:55 +0300

    modified task2. short version

--- task2.rb
+++ task2.rb
@@ -1,28 +1,18 @@
-a=[]
-a[0]=0
-a[1]=1
-a[2]=1
+a=[0,1,1]
 n = ARGV[0].to_i
-if n>=0
 n1 = a[1]
 n2 = a[2]
-if n<=2
- puts a[n]
+if n.abs<=2
+ n2 = a[n.abs]
 else
  counter = 3
- while counter<=n
+ while counter<=n.abs
  n1,n2 = n2,n1+n2
  counter+=1
  end
+end
+if n>=0
  puts n2
-end
 else
-counter = -1
-n1 = a[0]
-n2 = a[1]
-while counter>=n
-n1,n2=n2-n1,n1
-counter-=1
-end
-puts n1
+ puts ((-1)**(n-1))*n2
 end
